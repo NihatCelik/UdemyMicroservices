4520444380019641d62ea860e2ba2a4f5e84dc4e
Nihat Çelik <nc.nihatcelik@hotmail.com>
2022-01-09 08:52:35 +0300

    AppSettings added

--- FreeCourse.Services.Catalog/Startup.cs
+++ FreeCourse.Services.Catalog/Startup.cs
@@ -1,3 +1,4 @@
+using FreeCourse.Services.Catalog.Settings;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -5,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
@@ -28,6 +30,14 @@ namespace FreeCourse.Services.Catalog
             services.AddAutoMapper(typeof(Startup));
 
             services.AddControllers();
+
+            services.Configure<DatabaseSettings>(Configuration.GetSection("DatabaseSettings"));
+
+            services.AddSingleton<IDatabaseSettings>(sp =>
+            {
+                return sp.GetRequiredService<IOptions<DatabaseSettings>>().Value;
+            });
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "FreeCourse.Services.Catalog", Version = "v1" });
