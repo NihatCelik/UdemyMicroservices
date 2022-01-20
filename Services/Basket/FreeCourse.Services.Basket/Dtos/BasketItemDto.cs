011445be6712733fd8d870d220959884d2009360
Nihat Çelik <nc.nihatcelik@hotmail.com>
2022-01-20 07:27:45 +0300

    IBasketService and basketDtos added

---
+++ Services/Basket/FreeCourse.Services.Basket/Dtos/BasketItemDto.cs
@@ -0,0 +1,10 @@
+﻿namespace FreeCourse.Services.Basket.Dtos
+{
+    public class BasketItemDto
+    {
+        public string CourseId { get; set; }
+        public string CourseName { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
