037d50f094f3c1681c18cb1edb3fdf070ecd4ae1
Nihat Çelik <nc.nihatcelik@hotmail.com>
2022-02-03 08:27:20 +0300

    Address, Order And OrderItem classes created

---
+++ Services/Order/FreeCourse.Services.Order.Domain/OrderAggregate/Order.cs
@@ -0,0 +1,37 @@
+﻿using FreeCourse.Services.Order.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FreeCourse.Services.Order.Domain.OrderAggregate
+{
+    public class Order : Entity, IAggregateRoot
+    {
+        public string BuyerId { get; private set; }
+        public Address Address { get; private set; }
+        public DateTime CreatedDate { get; private set; }
+        
+        private readonly List<OrderItem> _orderItems;
+        public IReadOnlyCollection<OrderItem> OrderItems => _orderItems;
+
+        public Order(string buyerId, Address address)
+        {
+            BuyerId = buyerId;
+            Address = address;
+            _orderItems = new List<OrderItem>();
+            CreatedDate = DateTime.Now;
+        }
+        
+        public decimal GetTotalPrice => _orderItems.Sum(u => u.Price);
+
+        public void AddOrderItem(string productId, string productName, decimal price, string pictureUrl)
+        {
+            var existProduct = _orderItems.Any(u => u.ProductId == productId);
+            if (!existProduct)
+            {
+                var newOrderItem = new OrderItem(productId, productName, pictureUrl, price);
+                _orderItems.Add(newOrderItem);
+            }
+        }
+    }
+}
