d2697c5af1ec621a1b87d28021d7e4d56b3392c2
Nihat Çelik <nc.nihatcelik@hotmail.com>
2022-02-06 15:26:33 +0300

    GetOrdersByUserIdQuery added

---
+++ Services/Order/FreeCourse.Services.Order.Application/Handlers/GetOrdersByUserIdQueryHandler.cs
@@ -0,0 +1,38 @@
+﻿using FreeCourse.Services.Order.Application.Dtos;
+using FreeCourse.Services.Order.Application.Mapping;
+using FreeCourse.Services.Order.Application.Queries;
+using FreeCourse.Services.Order.Infrastructure;
+using FreeCourse.Shared.Dtos;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Order.Application.Handlers
+{
+    public class GetOrdersByUserIdQueryHandler : IRequestHandler<GetOrdersByUserIdQuery, Response<List<OrderDto>>>
+    {
+        public readonly OrderDbContext _context;
+
+        public GetOrdersByUserIdQueryHandler(OrderDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Response<List<OrderDto>>> Handle(GetOrdersByUserIdQuery request, CancellationToken cancellationToken)
+        {
+            var orders = await _context.Orders.Include(u => u.OrderItems).Where(x => x.BuyerId == request.UserId).ToListAsync();
+
+            if (!orders.Any())
+            {
+                return Response<List<OrderDto>>.Success(new List<OrderDto>(), 200);
+            }
+
+            var ordersDto = ObjectMapping.Mapper.Map<List<OrderDto>>(orders);
+
+            return Response<List<OrderDto>>.Success(ordersDto, 200);
+        }
+    }
+}
---
+++ Services/Order/FreeCourse.Services.Order.Application/Queries/GetOrdersByUserIdQuery.cs
@@ -0,0 +1,12 @@
+﻿using FreeCourse.Services.Order.Application.Dtos;
+using FreeCourse.Shared.Dtos;
+using MediatR;
+using System.Collections.Generic;
+
+namespace FreeCourse.Services.Order.Application.Queries
+{
+    public class GetOrdersByUserIdQuery : IRequest<Response<List<OrderDto>>>
+    {
+        public string UserId { get; set; }
+    }
+}
