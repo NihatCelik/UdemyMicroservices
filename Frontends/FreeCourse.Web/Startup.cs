dfe3e38163f6f5c0bedfba9e3d96f08b22542ecb
Nihat Çelik <nc.nihatcelik@hotmail.com>
2022-02-24 13:43:23 +0300

    Profil Page Added

--- Frontends/FreeCourse.Web/Startup.cs
+++ Frontends/FreeCourse.Web/Startup.cs
@@ -29,6 +29,7 @@ namespace FreeCourse.Web
 
             var serviceApiSettings = Configuration.GetSection("ServiceApiSettings").Get<ServiceApiSettings>();
 
+            services.AddScoped<ResourceOwnerPasswordTokenHandler>();
             services.AddHttpClient<IIdentityService, IdentityService>();
             services.AddHttpClient<IUserService, UserService>(opt =>
             {
