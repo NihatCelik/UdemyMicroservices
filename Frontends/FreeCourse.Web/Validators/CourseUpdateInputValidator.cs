0e6546cd5cc4248aae7a5633591b18df3fa3e85d
Nihat Çelik <nc.nihatcelik@hotmail.com>
2023-02-13 20:34:44 +0300

    FluentValidator added

---
+++ Frontends/FreeCourse.Web/Validators/CourseUpdateInputValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using FreeCourse.Web.Models.Catalogs;
+
+namespace FreeCourse.Web.Validators
+{
+    public class CourseUpdateInputValidator : AbstractValidator<CourseUpdateInput>
+    {
+        public CourseUpdateInputValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("isim alanı boş olamaz");
+            RuleFor(x => x.Description).NotEmpty().WithMessage("açıklama alanı boş olamaz");
+            RuleFor(x => x.Feature.Duration).InclusiveBetween(1, int.MaxValue).WithMessage("süre alanı boş olamaz");
+
+            RuleFor(x => x.Price).NotEmpty().WithMessage("fiyat alanı boş olamaz").ScalePrecision(2, 6).WithMessage("hatalı para formatı");
+        }
+    }
+}
